d8b3269a47f623ee0d061379a936221971b1942d
yuan_wy_wang <631824375@qq.com>
2021-04-24 16:37:09 +0800

    单词拼写错误

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import HttpRequest from './axios'
 import config from '@/config/index'
 console.log(process.env.NODE_ENV, 'env')
-const baseURL = process.env.NODE_ENV === 'prodcution' ? config.baseURL.prod : config.baseURL.dev
+const baseURL = process.env.NODE_ENV === 'production' ? config.baseURL.prod : config.baseURL.dev
 const axios = new HttpRequest(baseURL)
 
 export default axios
